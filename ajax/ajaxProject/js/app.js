e3948b25f1c131a93f0b7bddfa3be9b02c8413a7
TamaraMarr <tasha.m.bg@gmail.com>
2018-01-11 16:12:42 +0100

    BitShow partially stilized

--- ajax/ajaxProject/js/app.js
+++ ajax/ajaxProject/js/app.js
@@ -4,23 +4,23 @@ var request = $.ajax({
     url: 'http://api.tvmaze.com/shows'
 });
 
+// home page
 request.done(function (allShowInfo) {
     for (var i = 0; i < 50; i++) {
 
         let showName = allShowInfo[i].name;
         let showIMG = allShowInfo[i].image.original;
 
-        let articleDiv = $('<div>');
+        let articleDiv = $('<div class="showDiv">');
         articleDiv.addClass('col-sm-12 col-md-6 col-lg-4 col-xl-4');
 
-        let showImage = $('<img>');
+        let showImage = $('<img class="mainPage-showImg">');
         showImage.attr('src', showIMG);
         showImage.css('display', 'inline-block')
 
         let showLink = $('<a class="showLink">' + showName + '</a>');
         showLink.attr('data-show-id', (i + 1));
         showLink.attr('href', '#');
-
 
         articleDiv.append(showImage);
         articleDiv.append(showLink);
@@ -29,30 +29,78 @@ request.done(function (allShowInfo) {
     }
 });
 
+//switching to the single page
 $(document).on('click', '.showLink', function (event) {
     var usedLink = $(this).attr('data-show-id');
     localStorage.setItem('data-show-id', usedLink);
 
     window.location.href = 'single.html';
-
-    // const request = $.ajax({
-    //     url: 'http://api.tvmaze.com/shows'
-    // });
-
-    // request.done(function (selectedShow) {
-    //     let singleShow = null;
-    //     for (let i = 0; i < 50; i++) {
-    //         // console.log(selectedShow[i]);
-    //         if ($(selectedShow[i]).id === usedLink) {
-    //             singleShow = selectedShow[i];
-    //             console.log(singleShow);
-    //             break;
-    //         }
-    //     }
-
-    //     const showName = singleShow.name;
-    //     let title = $('<h1>');
-    //     const ffs = title.text(showName);
-    //     $('.singlShow').append(ffs);
-
 });
+
+function searchShows() {
+    let enteredText = $('.searchLine').val();
+
+    var request = $.ajax({
+        url: 'http://api.tvmaze.com/search/shows?q=' + enteredText
+    })
+
+    request.done(function (gotResult) {
+        console.log(gotResult);
+        $('.row').text('');
+
+        for (var i = 0; i < gotResult.length; i++) {
+
+            let showName = gotResult[i].show.name;
+            let showIMG;
+            let searchedURL = gotResult[i].show.id;
+
+            if (gotResult[i].show.image === null) {
+                showIMG = 'http://via.placeholder.com/350x500?text=No+Image+Available';
+            } else {
+                showIMG = gotResult[i].show.image.original;
+            }
+
+            $('.row').append(`<div class="col-sm-12 col-md-6 col-lg-4 col-xl-4">
+                                <div style="margin-bottom: 5px">
+                                    <img src='${showIMG}', style="display:inline-block">
+                                    <a class="showLink" data-show-id="${searchedURL}", href="#">${showName}</a>
+                                </div>
+                            </div>`);
+        }
+    })
+}
+
+//search line
+$(document).on('keydown', '.searchLine', function (event) {
+    if (event.keyCode === 13) {
+        searchShows();
+    }
+})
+
+//search dropdown list
+$('.searchLine').keyup(function() {
+    const enteredText = $('.searchLine').val();
+
+    var request = $.ajax({
+        url: 'http://api.tvmaze.com/search/shows?q=' + enteredText
+    });
+
+    request.done(function (gotResult) {
+        // console.log(gotResult);
+        for(let i = 0; i < gotResult.length; i++) {
+            let searchListItem = $('<li>');
+            searchListItem.append(gotResult[i].show.name);
+            searchListItem.attr('data-show-id', gotResult[i].show.id);
+            $('#searchList').append(searchListItem);
+        }
+
+        $('li').on('click', function() {
+            var usedLink = $(this).attr('data-show-id');
+            localStorage.setItem('data-show-id', usedLink);
+        
+            window.location.href = 'single.html';
+        })
+    });
+
+    $('#searchList').text('');
+})
--- ajax/ajaxProject/js/singleApp.js
+++ ajax/ajaxProject/js/singleApp.js
@@ -1,24 +1,148 @@
+let showID = localStorage.getItem("data-show-id");
+
 const request = $.ajax({
-    url: 'http://api.tvmaze.com/shows/'
+    url: 'http://api.tvmaze.com/shows/' + showID,
+    data: {
+        embed: ['seasons', 'cast']
+    }
 });
 
-request.done(function (shows) {
-    let showID = localStorage.getItem("data-show-id");
-    let singleShow;
+request.done(function (singleShow) {
+    // h1
+    let showTitle = singleShow.name;
+    let header = $(`<h1 class="singleShowTitle"> ${showTitle} </h1>`);
+    $('.container').append(header);
 
-    for (let i = 0; i < 50; i++) {
-        if ($(shows[i]).id === showID) {
-            singleShow = shows[i];
-            console.log(singleShow);
-            break;
+    // pic
+    if (singleShow.image === null) {
+        showPic = 'http://via.placeholder.com/350x500?text=No+Image+Available';
+    } else {
+        showPic = singleShow.image.original;
+    }
+    
+    // number of seasons
+    let numOfSeasons = singleShow._embedded.seasons.length;
+
+    // summary
+    let summary = singleShow.summary;
+
+    //list of seasons beginning and end dates
+
+    let seasonsDates = '';
+    let showCast = '';
+
+    for (let i = 0; i < singleShow._embedded.seasons.length; i++) {
+        if (singleShow._embedded.seasons[i].premiereDate === null || singleShow._embedded.seasons[i].endDate === null) {
+            seasonsDates += "<li>Unknown</li>"
+        } else {
+            seasonsDates += `<li>${singleShow._embedded.seasons[i].premiereDate}
+                            -${singleShow._embedded.seasons[i].endDate}</li>`;
         }
     }
 
-    // let showTitle = singleShow.name;
-    // console.log(showTitle);
+    for (let i = 0; i < singleShow._embedded.cast.length; i++) {
+        if (i === 10) {
+            break;
+        }
+        showCast += `<li>${singleShow._embedded.cast[i].person.name}</li>`
+    }
 
-    // const showName = singleShow.name;
-    // let title = $('<h1>');
-    // const ffs = title.text(showName);
-    // $('.singlShow').append(ffs);
+    // adding showPic, number of seasons and summary to bootstrap div
+    let bootstrapDiv = `<div class='row'>
+                            <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 singleShowImgDiv"><img src='`
+                                + showPic +
+                                `' class="singleShowImg" ></div>
+                            <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6"><h2 class="titleStyle">Seasons (`
+                                    + numOfSeasons +
+                                `)</h2>
+                                <ol>`
+                                    + seasonsDates +
+                                `</ol>
+                            <h2 class="titleStyle">Cast</h2>
+                                <ul>`
+                                    + showCast +
+                                `</ul>
+                            </div>
+                            <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 showDescription">` + summary + `</div>
+                        </div>`;
+
+    $('.container').append(bootstrapDiv);
 });
+
+//switching to the single page
+$(document).on('click', '.showLink', function (event) {
+    var usedLink = $(this).attr('data-show-id');
+    localStorage.setItem('data-show-id', usedLink);
+
+    window.location.href = 'single.html';
+});
+
+function searchShows() {
+    let enteredText = $('.searchLine').val();
+
+    var request = $.ajax({
+        url: 'http://api.tvmaze.com/search/shows?q=' + enteredText
+    })
+
+    request.done(function (gotResult) {
+        // console.log(gotResult);
+        $('.row').text('');
+        let rowDiv = $('<div>');
+        rowDiv.addClass('row');
+
+        for (var i = 0; i < gotResult.length; i++) {
+
+            let showName = gotResult[i].show.name;
+            let showIMG;
+            let searchedURL = gotResult[i].show.id;
+
+            if (gotResult[i].show.image === null) {
+                showIMG = 'http://via.placeholder.com/350x500?text=No+Image+Available';
+            } else {
+                showIMG = gotResult[i].show.image.original;
+            }
+
+            $('.row').append(`<div class="col-sm-12 col-md-6 col-lg-4 col-xl-4">
+                                <div style="margin-bottom: 5px">
+                                    <img src='${showIMG}'>
+                                    <a class="showLink" data-show-id="${searchedURL}", href="#">${showName}</a>
+                                </div>
+                            </div>`);
+        }
+    })
+}
+
+//search line
+$(document).on('keydown', '.searchLine', function (event) {
+    if (event.keyCode === 13) {
+        searchShows();
+    }
+})
+
+//search dropdown list
+$('.searchLine').keyup(function() {
+    const enteredText = $('.searchLine').val();
+
+    var request = $.ajax({
+        url: 'http://api.tvmaze.com/search/shows?q=' + enteredText
+    });
+
+    request.done(function (gotResult) {
+        console.log(gotResult);
+        for(let i = 0; i < gotResult.length; i++) {
+            let searchListItem = $('<li>');
+            searchListItem.append(gotResult[i].show.name);
+            searchListItem.attr('data-show-id', gotResult[i].show.id);
+            $('#searchList').append(searchListItem);
+        }
+
+        $('li').on('click', function() {
+            var usedLink = $(this).attr('data-show-id');
+            localStorage.setItem('data-show-id', usedLink);
+        
+            window.location.href = 'single.html';
+        })
+    });
+
+    $('#searchList').text('');
+})
