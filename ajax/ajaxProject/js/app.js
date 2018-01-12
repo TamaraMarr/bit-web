78028351a62c2c380386eda473625be0ff638ad9
TamaraMarr <tasha.m.bg@gmail.com>
2018-01-12 15:12:43 +0100

    App stylized, search fixed

--- ajax/ajaxProject/js/app.js
+++ ajax/ajaxProject/js/app.js
@@ -1,4 +1,5 @@
-let articleContainer = $('.row');
+const articleContainer = $('.row');
+$("#searchList").css("border", "none");
 
 var request = $.ajax({
     url: 'http://api.tvmaze.com/shows'
@@ -7,19 +8,18 @@ var request = $.ajax({
 // home page
 request.done(function (allShowInfo) {
     for (var i = 0; i < 50; i++) {
-
         let showName = allShowInfo[i].name;
         let showIMG = allShowInfo[i].image.original;
+        let showID = allShowInfo[i].id;
 
         let articleDiv = $('<div class="showDiv">');
         articleDiv.addClass('col-sm-12 col-md-6 col-lg-4 col-xl-4');
 
-        let showImage = $('<img class="mainPage-showImg">');
-        showImage.attr('src', showIMG);
+        let showImage = $(`<a href="#"><img src=${showIMG} data-show-id=${showID} class="mainPage-showImg"></a>`);
         showImage.css('display', 'inline-block')
 
         let showLink = $('<a class="showLink">' + showName + '</a>');
-        showLink.attr('data-show-id', (i + 1));
+        showLink.attr('data-show-id', (showID));
         showLink.attr('href', '#');
 
         articleDiv.append(showImage);
@@ -27,9 +27,12 @@ request.done(function (allShowInfo) {
 
         articleContainer.append(articleDiv);
     }
+
+    let header = $(`<h1 class="col-12 mainTitle">Popular, but not Necessarily Good Shows</h1>`);
+    $(".row").prepend(header);
 });
 
-//switching to the single page
+//switching to the single page from show name
 $(document).on('click', '.showLink', function (event) {
     var usedLink = $(this).attr('data-show-id');
     localStorage.setItem('data-show-id', usedLink);
@@ -37,70 +40,10 @@ $(document).on('click', '.showLink', function (event) {
     window.location.href = 'single.html';
 });
 
-function searchShows() {
-    let enteredText = $('.searchLine').val();
+//switching to the single page from show image
+$(document).on('click', '.mainPage-showImg', function (event) {
+    var usedLink = $(this).attr('data-show-id');
+    localStorage.setItem('data-show-id', usedLink);
 
-    var request = $.ajax({
-        url: 'http://api.tvmaze.com/search/shows?q=' + enteredText
-    })
-
-    request.done(function (gotResult) {
-        console.log(gotResult);
-        $('.row').text('');
-
-        for (var i = 0; i < gotResult.length; i++) {
-
-            let showName = gotResult[i].show.name;
-            let showIMG;
-            let searchedURL = gotResult[i].show.id;
-
-            if (gotResult[i].show.image === null) {
-                showIMG = 'http://via.placeholder.com/350x500?text=No+Image+Available';
-            } else {
-                showIMG = gotResult[i].show.image.original;
-            }
-
-            $('.row').append(`<div class="col-sm-12 col-md-6 col-lg-4 col-xl-4">
-                                <div style="margin-bottom: 5px">
-                                    <img src='${showIMG}', style="display:inline-block">
-                                    <a class="showLink" data-show-id="${searchedURL}", href="#">${showName}</a>
-                                </div>
-                            </div>`);
-        }
-    })
-}
-
-//search line
-$(document).on('keydown', '.searchLine', function (event) {
-    if (event.keyCode === 13) {
-        searchShows();
-    }
-})
-
-//search dropdown list
-$('.searchLine').keyup(function() {
-    const enteredText = $('.searchLine').val();
-
-    var request = $.ajax({
-        url: 'http://api.tvmaze.com/search/shows?q=' + enteredText
-    });
-
-    request.done(function (gotResult) {
-        // console.log(gotResult);
-        for(let i = 0; i < gotResult.length; i++) {
-            let searchListItem = $('<li>');
-            searchListItem.append(gotResult[i].show.name);
-            searchListItem.attr('data-show-id', gotResult[i].show.id);
-            $('#searchList').append(searchListItem);
-        }
-
-        $('li').on('click', function() {
-            var usedLink = $(this).attr('data-show-id');
-            localStorage.setItem('data-show-id', usedLink);
-        
-            window.location.href = 'single.html';
-        })
-    });
-
-    $('#searchList').text('');
-})
+    window.location.href = 'single.html';
+});
--- ajax/ajaxProject/js/singleApp.js
+++ ajax/ajaxProject/js/singleApp.js
@@ -1,6 +1,9 @@
-let showID = localStorage.getItem("data-show-id");
+$("#searchList").css("border", "none");
 
-const request = $.ajax({
+const getSingleShow = (() => {
+    let showID = localStorage.getItem("data-show-id");
+
+    request = $.ajax({
     url: 'http://api.tvmaze.com/shows/' + showID,
     data: {
         embed: ['seasons', 'cast']
@@ -10,8 +13,8 @@ const request = $.ajax({
 request.done(function (singleShow) {
     // h1
     let showTitle = singleShow.name;
-    let header = $(`<h1 class="singleShowTitle"> ${showTitle} </h1>`);
-    $('.container').append(header);
+    let header = $(`<h1 class="col-12 singleShowTitle"> ${showTitle} </h1>`);
+    $('.row').prepend(header);
 
     // pic
     if (singleShow.image === null) {
@@ -48,8 +51,7 @@ request.done(function (singleShow) {
     }
 
     // adding showPic, number of seasons and summary to bootstrap div
-    let bootstrapDiv = `<div class='row'>
-                            <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 singleShowImgDiv"><img src='`
+    let bootstrapDiv = `<div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 singleShowImgDiv"><img src='`
                                 + showPic +
                                 `' class="singleShowImg" ></div>
                             <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6"><h2 class="titleStyle">Seasons (`
@@ -63,86 +65,12 @@ request.done(function (singleShow) {
                                     + showCast +
                                 `</ul>
                             </div>
-                            <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 showDescription">` + summary + `</div>
-                        </div>`;
+                            <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 showDescription">` + summary + `</div>`;
 
-    $('.container').append(bootstrapDiv);
-});
-
-//switching to the single page
-$(document).on('click', '.showLink', function (event) {
-    var usedLink = $(this).attr('data-show-id');
-    localStorage.setItem('data-show-id', usedLink);
-
-    window.location.href = 'single.html';
-});
-
-function searchShows() {
-    let enteredText = $('.searchLine').val();
-
-    var request = $.ajax({
-        url: 'http://api.tvmaze.com/search/shows?q=' + enteredText
-    })
-
-    request.done(function (gotResult) {
-        // console.log(gotResult);
-        $('.row').text('');
-        let rowDiv = $('<div>');
-        rowDiv.addClass('row');
-
-        for (var i = 0; i < gotResult.length; i++) {
-
-            let showName = gotResult[i].show.name;
-            let showIMG;
-            let searchedURL = gotResult[i].show.id;
-
-            if (gotResult[i].show.image === null) {
-                showIMG = 'http://via.placeholder.com/350x500?text=No+Image+Available';
-            } else {
-                showIMG = gotResult[i].show.image.original;
-            }
-
-            $('.row').append(`<div class="col-sm-12 col-md-6 col-lg-4 col-xl-4">
-                                <div style="margin-bottom: 5px">
-                                    <img src='${showIMG}'>
-                                    <a class="showLink" data-show-id="${searchedURL}", href="#">${showName}</a>
-                                </div>
-                            </div>`);
-        }
-    })
-}
-
-//search line
-$(document).on('keydown', '.searchLine', function (event) {
-    if (event.keyCode === 13) {
-        searchShows();
-    }
+    $('.row').append(bootstrapDiv);
 })
 
-//search dropdown list
-$('.searchLine').keyup(function() {
-    const enteredText = $('.searchLine').val();
+localStorage.removeItem("data-show-id")
+});
 
-    var request = $.ajax({
-        url: 'http://api.tvmaze.com/search/shows?q=' + enteredText
-    });
-
-    request.done(function (gotResult) {
-        console.log(gotResult);
-        for(let i = 0; i < gotResult.length; i++) {
-            let searchListItem = $('<li>');
-            searchListItem.append(gotResult[i].show.name);
-            searchListItem.attr('data-show-id', gotResult[i].show.id);
-            $('#searchList').append(searchListItem);
-        }
-
-        $('li').on('click', function() {
-            var usedLink = $(this).attr('data-show-id');
-            localStorage.setItem('data-show-id', usedLink);
-        
-            window.location.href = 'single.html';
-        })
-    });
-
-    $('#searchList').text('');
-})
+getSingleShow();
