717f6d8ed95cd210678bed70b30bbf132d142389
TamaraMarr <tasha.m.bg@gmail.com>
2018-02-20 23:18:50 +0100

    Bootstrap example for gen2

---
+++ cssreacttesting/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+		<div className="DottedBox">
+			<p className="DottedBox_content">Get started with CSS styling</p>
+		</div>
+    );
+  }
+}
+
+export default App;
